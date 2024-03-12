51f74000cfde0382a3bea9737e7c16f62ccc363f
ManuelMaisano <manumaisano@gmail.com>
2024-03-12 14:10:10 +0100

    change tips js

--- array_tail/script.js
+++ array_tail/script.js
@@ -1,2 +1,3 @@
-//Crea due array che hanno un numero di elementi diversi.
-//Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+//Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri casuali 
+//(compresi nell'intervallo da 1 a 100) per quanti sono gli elementi da inserire.
+//Stampiamo poi gli ultimi 5 elementi dell'Array
