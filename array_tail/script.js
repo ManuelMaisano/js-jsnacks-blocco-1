37e942ec93ffc06ce0e4f2c361a11c9f2adc6dd2
Manuel Maisano <manumaisano@gmail.com>
2024-03-18 11:57:28 +0100

    ??

--- array_tail/script.js
+++ array_tail/script.js
@@ -3,6 +3,18 @@
 //Stampiamo poi gli ultimi 5 elementi dell'Array
 
 let usernumber = parseInt(prompt("Inserire quanti numeri dovrà contenere l'array"));
+console.log(usernumber)
+let numberToPrint = parseInt(prompt("Quanti elementi dell'array vuoi stamapare?"));
 
 let emptyArray =[];
+
+if(numberToPrint > usernumber) {
+    console.log("Errore, non puoi stamapare più elementi di quelli contenuti nell'insieme.");
+} else {
+    for(let i = 0; i < usernumber; i++) {
+        let rndNumber =  getRndInteger(1, 100);
+        emptyArray.push(rndNumber);
+    } 
+    console.log(emptyArray.slice(-numberToPrint));
+}
 
